0a573ba7ed176159667c9f12a7eba6f5144f9535
phuong.ta <phuong.ta@tu-dortmund.de>
2024-02-18 14:57:42 +0100

    okok

--- commands.js
+++ commands.js
@@ -12,13 +12,13 @@ let listCommands = [
         "name": "habit name",
         "description": "Something you want to track. Example: drink 1l water",
         "type": 3,
-        "required": True,
+        "required": true,
       },
       {
         "name": "duration",
         "description": "Set a duration. Default 30 days.",
         "type": 4,
-        "required": False,
+        "required": false,
       }
     ]
   }
