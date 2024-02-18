2bcc515af601b2049b60ca27e5e214cfbb04c779
phuong.ta <phuong.ta@tu-dortmund.de>
2024-02-18 14:47:05 +0100

    test

--- index.js
+++ index.js
@@ -89,24 +89,6 @@ app.get('/register_commands', async (req,res) => {
       "name": "hello",
       "description": "replies with Hallo!",
       "options": []
-    },
-    {
-      "name": "add",
-      "description": "Add a habit to track (●'◡'●)",
-      "options": [
-        {
-          "name": "habit name",
-          "description": "Something you want to track. Example: drink 1l water",
-          "type": 3,
-          "required": True,
-        },
-        {
-          "name": "duration",
-          "description": "Set a duration. Default 30 days.",
-          "type": 4,
-          "required": False,
-        }
-      ]
     }
   ]
   try
